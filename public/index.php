12ac89fe746e321516d579f3b26aa92aa0e29c8d
Omar Sood <goatfryed@gmail.com>
2019-05-15 11:51:29 +0200

    Hello php programming

--- public/index.php
+++ public/index.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Hello World!</title>
-    <link rel="icon" type="image/png" href="favicon.png">
-</head>
-<body>
-Hello World from php, docker &amp; nginx!
-</body>
-</html>
+<?php
+    $php = "php-".PHP_VERSION;
+    echo <<<HTML
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Hello World!</title>
+                <link rel="icon" type="image/png" href="favicon.png">
+            </head>
+            <body>
+                Hello World from {$php}, docker &amp; nginx!
+            </body>
+        </html>
+    HTML;
