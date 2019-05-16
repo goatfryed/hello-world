ae4a0dedcc3b385c8d2ff651f788dfa61eacf81e
Omar Sood <omar.sood@onquality.de>
2019-05-16 15:22:20 +0200

    hello state

--- hello-js/src/app.tsx
+++ hello-js/src/app.tsx
@@ -1,20 +1,35 @@
 import {render} from "react-dom";
 import * as React from "react";
+import {createStore} from "redux";
+import {Provider, connect} from "react-redux";
 
-const App = () => <div className="container">
+const defaultMessage = "Hello, World, from react, redux, typescript, javascript, webpack, nginx & docker!";
+const appReducer = (state = defaultMessage, action: any): string => state;
+
+const Message = ({message}: {message: string}) => <div className="container">
     <h1 className="title">
-        Hello, World, from react, typescript, javascript, webpack, nginx & docker!
+        {message}
     </h1>
     <p className="subtitle">
         Definitely just a simple hello.
     </p>
 </div>;
 
+const App = connect(
+    (state: string) => ({message: state})
+)(Message);
+
 window.addEventListener(
     "DOMContentLoaded",
     function () {
+        const store = createStore(
+            appReducer
+        );
+
         render(
-            <App />,
+            <Provider store={store}>
+                <App  />
+            </Provider>,
             document.getElementById("app")
         );
     }
