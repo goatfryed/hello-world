5ecc3b264e833879d27a4ac1042be6a1a573c02b
Omar Sood <goatfryed@gmail.com>
2019-05-27 08:58:10 +0200

    Hello graphql in js

--- hello-js/src/app.tsx
+++ hello-js/src/app.tsx
@@ -2,10 +2,27 @@ import {render} from "react-dom";
 import * as React from "react";
 import {initialMessage} from "./Message";
 import FunctionalMessageViewer from "./FunctionalMessageViewer";
+import ApolloClient, {gql} from "apollo-boost";
 
 window.addEventListener(
     "DOMContentLoaded",
     function () {
+        const client = new ApolloClient({
+           uri: "http://hello.php.test/graphql/"
+        });
+
+        client.query({
+            query: gql`
+               query {
+                  getMessageSupporter(message: 1) {
+                    oneliner
+                  }
+               }
+            `
+        }).then(
+            result => console.log(result.data.getMessageSupporter.oneliner)
+        );
+
         render(
             <FunctionalMessageViewer initialMessage={initialMessage}/>,
             document.getElementById("app")
